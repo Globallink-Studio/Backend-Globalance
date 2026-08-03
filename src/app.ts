5dff9eab718d593dc5eb5804b9cd367927178264
noejazmin <noejazminmrivas@gmail.com>
2026-08-03 11:15:27 -0300

    docs: add Swagger API documentation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,20 @@
 import express from "express";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
 import routes from "./routes";
+import { swaggerSpec } from "./config/swagger";
 import { errorHandler } from "./middlewares/error.middleware";
 
 export const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.get("/api/docs.json", (_req, res) => {
+  res.json(swaggerSpec);
+});
 
 app.use("/api", routes);
 
