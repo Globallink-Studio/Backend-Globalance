8b51cc06275e084b98cd29b2074bba9b3202f696
Manuela <manuelahenaod19@gmail.com>
2026-08-08 21:12:47 -0500

    feat(users): make PATCH /users/profile an upsert and persist display_currency

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -3,8 +3,9 @@ import { AppError } from "../../errors/app-error";
 import { findUserByFirebaseUid } from "../auth/auth.repository";
 import {
   findUserWithProfileByFirebaseUid,
-  createCompanyProfile,
-  createPersonProfile,
+  upsertCompanyProfile,
+  upsertPersonProfile,
+  updateUserDisplayCurrency,
   updateUserType,
   updateWalletAlias,
   UserType,
@@ -17,6 +18,7 @@ type CompletePersonProfile = {
   document: string;
   phone: string;
   alias: string;
+  displayCurrency: "ARS" | "USD" | "EUR";
 };
 
 type CompleteCompanyProfile = {
@@ -25,6 +27,7 @@ type CompleteCompanyProfile = {
   document: string;
   phone: string;
   alias: string;
+  displayCurrency: "ARS" | "USD" | "EUR";
 };
 
 export type CompleteProfileParams =
@@ -50,6 +53,17 @@ export async function completeProfile(
       );
     }
 
+    if (
+      user.user_type &&
+      user.user_type !== profile.userType
+    ) {
+      throw new AppError(
+        409,
+        "USER_TYPE_NOT_EDITABLE",
+        "El tipo de usuario no puede modificarse.",
+      );
+    }
+
     await updateUserType(client, {
       firebaseUid,
       userType: profile.userType,
@@ -60,8 +74,13 @@ export async function completeProfile(
       alias: profile.alias,
     });
 
+    await updateUserDisplayCurrency(client, {
+      userId: user.id,
+      displayCurrency: profile.displayCurrency,
+    });
+
     if (profile.userType === "person") {
-      await createPersonProfile(client, {
+      await upsertPersonProfile(client, {
         userId: user.id,
         firstName: profile.firstName,
         lastName: profile.lastName,
@@ -69,7 +88,7 @@ export async function completeProfile(
         phone: profile.phone,
       });
     } else {
-      await createCompanyProfile(client, {
+      await upsertCompanyProfile(client, {
         userId: user.id,
         legalName: profile.legalName,
         document: profile.document,
